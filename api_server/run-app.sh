5fe20d7ff1fcef8edbbbdbc416ba458d769ad1c6
Nick Hess <hessnt30@vt.edu>
2025-01-25 14:34:25 -0500

    added git repo to raspberry pi, and updated run script

--- api_server/run-app.sh
+++ api_server/run-app.sh
@@ -1,5 +1,18 @@
-tmux new-session -d -s api
-python3 -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
-python3 app.py
+#!/bin/bash
+
+tmux new-session -d -s api 
+tmux send-keys -t api "echo '[Run App] Created new session called api'" Enter
+
+tmux send-keys -t api "python3 -m venv venv" Enter
+tmux send-keys -t api "echo '[Run App] Created new virtual environment called venv'" Enter
+tmux send-keys -t api "source venv/bin/activate" Enter
+tmux send-keys -t api "echo '[Run App] Activated venv'" Enter
+
+tmux send-keys -t api "echo '[Run App] Installing dependencies...'" Enter
+tmux send-keys -t api "pip install -r requirements.txt" Enter
+tmux send-keys -t api "echo '[Run App] Dependencies installed'" Enter
+
+tmux send-keys -t api "echo '[Run App] Starting Flask App...'" Enter
+tmux send-keys -t api "python3 app.py" Enter
+
+tmux attach -t api
